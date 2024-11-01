f856acaa5788ef0bd183c7a4df81b39341a38261
sahib026 <osmanli.sahib@mail.ru>
2024-11-01 14:49:46 +0400

    add-js-task
    add-js-task

---
+++ js task function/function.js
@@ -0,0 +1,20 @@
+//2.----> 
+//İstifadəçi iki ədəd daxil etsin. Həmin ədədlərin toplanmasını və vurulmasını həyata keçirən bir function yazın.
+
+//function toplamavurma(){
+//let eded1 = prompt( birinci ededi daxil edin);
+//let eded2 = prompt( ikinci ededi daxil edin);
+
+//let toplama = eded1 + eded2;
+//let vurma = eded1 * eded2;
+
+//console.log("ededlerin cemi"+ toplama);
+//console.log("ededlerin hasili"+ vurma);
+
+}
+//4.----> 
+//İstifadəçidən bir username alın. username daxil edildikdən sonra həmin istifadəçini salamlayan bir bildiriş pəncərəsi açılsın. Bunun üçün bir function-dan istifadə edin.
+//function username(){
+   // let username = prompt("zehmet olmasa adinizi daxil edin");
+  //  alert ("salam",username,"!");
+}
